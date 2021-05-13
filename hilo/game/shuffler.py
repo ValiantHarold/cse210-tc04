6df8801de70e8acf3590d55f63190b78e86b9f51
ValiantWolf <wit19011@byui.edu>
2021-05-13 13:19:49 -0600

    comment changes

--- hilo/game/shuffler.py
+++ hilo/game/shuffler.py
@@ -1,24 +1,24 @@
 import random
 
-class Shuffle:
-    """Comment"""
+class Shuffler:
+    """Everything to do with the cards"""
 
     def __init__(self):
-        """Comment"""
+        """Stores the cards"""
 
         self.deck = list(range(1,14))
         self.old_card = 7
         self.new_card = 7
 
     def get_card(self, card_type):
-        """Comment"""
+        """Grabs a card and returns it"""
 
         if card_type == "old":
             return self.old_card
         return self.new_card
 
     def shuffle_deck(self):
-        """Comment"""
+        """Shuffles and picks a card"""
 
         self.old_card = self.new_card
         self.new_card = random.choice(self.deck)
@@ -28,7 +28,7 @@ class Shuffle:
         return self.new_card
 
     def get_points(self, user_input):
-        """Comment"""
+        """Gives points based on cards"""
         diff = self.old_card - self.new_card
         if diff < 0 and user_input == True or diff > 0 and user_input == False:
             return 100
